b5b66ee0ae7fc1848f414c6d005239c866678b59
Lefter Andrei <ionut.lefter@student.unitbv.ro>
2025-01-13 22:37:08 +0200

    Added styling and DB connection

--- Server/Routes/main.js
+++ Server/Routes/main.js
@@ -1,17 +1,47 @@
 const express = require('express');
 const router = express.Router();
-
+const Post = require('../Models/Post');
 
 //Routes
 
-router.get('', (req, res) => {
-    const locals = {
-        pageTitle : "Bloggr home",
-        description : "A simple blog web application, enabling you to share your deepests thoughts. Built upon the mighty NodeJS, with the help of legends like MongoDB and Express"
-    }
+/**
+ * GET/
+ * HOME
+ */
 
-    res.render('index', locals);
+router.get('', async (req, res) => {
+    
+    try {
+        const locals = {
+            pageTitle : "Bloggr home",
+            description : "A simple blog web application, enabling you to share your deepests thoughts. Built upon the mighty NodeJS, with the help of legends like MongoDB and Express"
+        }
+
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate( [ { $sort : { createdAt: -1}} ] )
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();  
+
+        const count = await Post.countDocuments();
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+        
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null;
+            });
+
+     } catch(error) {
+        console.log(error);
+     }
+
 });
+
 
 router.get('/about', (req, res) => {
     res.render('about')
@@ -21,4 +51,22 @@ router.get('/contact', (req, res) => {
     res.render('contact')
 })
 
+// function insertPostData () {
+//     Post.insertMany([
+//         {
+//             title: "Building a blog web app",
+//             body: "This is the body text"
+//         },
+//         {
+//             title: "Working on DB functionality",
+//             body: "Only testing if post works for now"
+//         },
+//         {
+//             title: "Hello world",
+//             body: "What's happening?"
+//         },
+        
+//     ])
+// }
+// insertPostData();
 module.exports = router;
--- app.js
+++ app.js
@@ -3,16 +3,21 @@ require ('dotenv').config();
 const express = require('express');
 const expressLayout = require('express-ejs-layouts');
 
+const connectDB = require('./Server/Config/db');
+
 const app = express();
 const PORT = process.env.PORT || 5000
 
+//Connect to DB
+
+connectDB();
 
 app.use(express.static('public'));
 
 //Templating Engine
 app.use(expressLayout);
 app.set('layout', './layouts/main');
-app.set('view engine', 'ejs');
+app.set('view engine', 'ejs'); 
 
 
 app.use('/', require('./Server/Routes/main'));
